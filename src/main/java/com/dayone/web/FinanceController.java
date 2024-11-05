863ec0961be96d94d5b51677f0dfefc046dac9ef
shCHO9801 <csh980116@naver.com>
2024-11-06 01:08:09 +0900

    서비스 구현 - 배당금 조회

--- src/main/java/com/dayone/web/FinanceController.java
+++ src/main/java/com/dayone/web/FinanceController.java
@@ -1,5 +1,7 @@
 package com.dayone.web;
 
+import com.dayone.service.FinanceService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/finance")
+@AllArgsConstructor
 public class FinanceController {
+
+    private FinanceService financeService;
+
+    /*
+    * 회사명에 따른 배당금 정보 조회
+    * */
     @GetMapping("/dividend/{companyName}")
     public ResponseEntity<?> searchFinance(@PathVariable String companyName) {
-        return null;
+        var result = this.financeService.getDividendByCompanyName(companyName);
+        return ResponseEntity.ok(result);
     }
 }
